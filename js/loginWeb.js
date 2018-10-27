8cd42e0ba6572a771f9a3462e1a10eceaae8b54b
irobotzt <Arm_pattinum@hotmail.com>
2018-10-27 18:03:14 +0700

    login_web

--- js/loginWeb.js
+++ js/loginWeb.js
@@ -1,20 +1,23 @@
-function login(){
+
+function login() {
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-  
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error : " + errorMessage);
+
+        // ...
     });
+    document.getElementById("user_div").style.display = "block";
+
   
-  }
-  function logout(){
+}
+function logout() {
     firebase.auth().signOut();
-  }
-  
+    
+}
