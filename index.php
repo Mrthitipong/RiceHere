d3c6892574d387ea81b341afee1f89ae19caf53b
Tiw.thitipong <mr.thitipongtiw@gmail.com>
2018-10-27 19:38:17 +0700

    create first page

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="css_layout.css">
+</head>
+<body>
+<div id="all">
+    <div id="header" align="center">คนสวย</h1></div>
+    <div id="left"><b>This is left side.</b>    
+    </div>  
+     <div id="content">
+           <p>การจัดวางโครงสร้าง (Layout) ของเว็บ ถือเป็นจุดเริ่มต้นและจุดสำคัญมากในการออกแบบ ก่อนที่จะสร้างส่วนอื่นๆ เว็บโปรแกรมเมอร์ส่วนใหญ่มักออกแบบเค้าโครงของเว็บเพจไว้ก่อนเสมอ จากนั้นจึงกำหนดโครงสร้างอื่นๆต่อไป
+            </p>
+            <p>ตัวอย่างนี้ผู้เขียนได้ออกแบบ Layout อย่างคร่าวๆ โดยใช้แท็ก DIV ใน HTML พร้อมกับใช้ CSS ในการจัดการให้ข้อมูลออกมาในรูปแบบที่ต้องการ
+            </p>
+       </div>
+       <div id="footer"><b>This is footer</b></div>
+</div>
+</body>
+</html>
