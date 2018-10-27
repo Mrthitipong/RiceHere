61e5680222d31dcff10cc90e0fd5111bfe9ca5b3
irobotzt <Arm_pattinum@hotmail.com>
2018-10-27 19:26:29 +0700

    Register_and_footer

--- js/loginWeb.js
+++ js/loginWeb.js
@@ -3,7 +3,14 @@ function login() {
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
