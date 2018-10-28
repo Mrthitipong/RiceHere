4bf6ac47f9afde01dd6aa6ddfd0fe949e8ee2459
irobotzt <Arm_pattinum@hotmail.com>
2018-10-28 08:24:29 +0700

    The_last_upload

--- index.php
+++ index.php
@@ -1,19 +1,179 @@
 <html>
+
 <head>
-<link type="text/css" rel="stylesheet" href="css_layout.css">
+
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap4/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main_styles.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.structure.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.theme.css">
 </head>
+
 <body>
-<div id="all">
-    <div id="header" align="center">คนสวย</h1></div>
-    <div id="left"><b>This is left side.</b>    
-    </div>  
-     <div id="content">
-           <p>การจัดวางโครงสร้าง (Layout) ของเว็บ ถือเป็นจุดเริ่มต้นและจุดสำคัญมากในการออกแบบ ก่อนที่จะสร้างส่วนอื่นๆ เว็บโปรแกรมเมอร์ส่วนใหญ่มักออกแบบเค้าโครงของเว็บเพจไว้ก่อนเสมอ จากนั้นจึงกำหนดโครงสร้างอื่นๆต่อไป
+
+
+    <div class="row" style="height:100%;background-image: url(images/salogan.jpg);background-size: 100% 100%;padding:8px;">
+
+
+        <div class="col-4"> </div>
+        <div class="col-4" style="text-align: center;">
+            <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+            <a href="tt/PJ/first_buy.php"><button type="button" class="btn btn-primary" style="width:150px;height:60px;">สั่งซื้อ</button></a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+
+            <button type="button" id="sell_user" class="btn btn-success" style="width:150px;height:60px;">ลงขาย
+            </button>
+
+
+            <div id="login_div" class="main-div">
+                <div id="box" title="Basic dialog">
+                    <div class="container">
+                        <label class="email"><b>อีเมล</b></label>
+                        <input class="email_user" id="email" type="email" name="uname">
+                        <br>
+                        <label class="pass"><b>รหัสผ่าน</b></label>
+                        <input class="password_user" id="password" type="password" name="password"><br>
+                        <div class="row">
+                            <div class="col-6" align="center" >
+                                <button  onclick="login()" id="Sign">เข้าสู่ระบบ</button><br></div>
+                             
+                            <div class="col-6">
+                                <button class="sign_up_f" align="center" id="Sign_up1">สมัครสมาชิก</button>
+                            </div>
+                            
+                        </div>
+                        <div class="row">
+                            <div class="col-6" align="center" >
+                        <button onclick="loginGoogle()" id="Sign_google" > GOOGLE </button><br><br></div>
+                        <div class="col-6">
+                                <button onclick="loginFacebook()" class="sign_up_f" align="center"  id="Sign_up1">FACEBOOK</button>
+                            </div>
+
+                    </div>
+                </div>
+            </div>
+            <div id="signup" title="Basic dialog">
+                <div class="container">
+                    <label class="email"><b>อีเมล</b></label>
+                    <input class="email_user" id="email_f" type="email" name="uname">
+                    <br>
+                    <label class="pass"><b>รหัสผ่าน</b></label>
+                    <input class="password_user" id="password_f" type="password" name="password"><br>
+                    <br>
+                    <label class="phon"><b>หมายเลขโทรศัพท์</b></label>
+                    <input class="Phone_number" id="Phone_number" type="text" name="Phone_number"><br>
+                    <button onclick="signup()" id="Sign_up">สมัครสมาชิก</button>
+                    <button style="display: none;" type="button" onclick="close()" id="close">ปิด</button>
+                </div>
+            </div>
+        </div>
+        <br><br>
+    </div>
+    <footer class="container-fluid text-center foot">
+        <div class="copyrights">
+            <img class="footimg" src="images/logo.png" alt="Logo">
+            <hr class="dark-line">
+            <h5 class="active">อีเมล : ricehere.buu@gmail.com </h5>
+            
+            <h5 class="active">โทร : 0853939908</h5>
+            <p class="white-txt small">
+                ซื้อง่าย ขายข้าว จากชาวนาคุณภาพ นึกถึง Rice Here
             </p>
-            <p>ตัวอย่างนี้ผู้เขียนได้ออกแบบ Layout อย่างคร่าวๆ โดยใช้แท็ก DIV ใน HTML พร้อมกับใช้ CSS ในการจัดการให้ข้อมูลออกมาในรูปแบบที่ต้องการ
-            </p>
-       </div>
-       <div id="footer"><b>This is footer</b></div>
-</div>
+        </div>
+    </footer>
+
 </body>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery-ui.js"></script>
+<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/loginWeb.js"></script>
+<script type="text/javascript" src="js/signUpWeb.js"></script>
+<script src="https://www.gstatic.com/firebasejs/5.5.6/firebase.js"></script>
+<script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCxzWdnO_3SJ_oIsjNWW5VHkK8_x3aomvI",
+        authDomain: "rice-here2018.firebaseapp.com",
+        databaseURL: "https://rice-here2018.firebaseio.com",
+        projectId: "rice-here2018",
+        storageBucket: "rice-here2018.appspot.com",
+        messagingSenderId: "560163102847"
+    };
+    firebase.initializeApp(config);
+    var provider = new firebase.auth.GoogleAuthProvider();
+    function loginGoogle() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+          var token = result.credential.accessToken;
+    
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    }
+      var provider = new firebase.auth.FacebookAuthProvider();
+        function loginFacebook() {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            var token = result.credential.accessToken;
+        
+            var user = result.user;
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    }
+</script>
+<script>
+    $(function () {
+        $("#box").dialog({
+            autoOpen: false,
+            title: 'กรุณากรอกข้อมูล',
+            width: 500,
+            height: 600,
+            modal: true,
+            resizable: false,
+            draggable: false,
+        });
+        $("#signup").dialog({
+            autoOpen: false,
+            title: 'กรุณากรอกข้อมูล',
+            width: 500,
+            height: 600,
+            modal: true,
+            resizable: false,
+            draggable: false,
+        });
+        $("#sell_user").on("click", function () {
+            $("#box").dialog("open");
+        });
+        $("#Sign").on("click", function () {
+            $("#box").dialog("close");
+        });
+        $("#Sign_up1").on("click", function () {
+            $("#box").dialog("close");
+        });
+        $("#Sign_up1").on("click", function () {
+            $("#signup").dialog("open");
+        });
+        $("#close").on("click", function () {
+            $("#signup").dialog("close");
+        });
+    });
+
+</script>
+
 </html>
--- tt/PJ/sales_report.php
+++ tt/PJ/sales_report.php
@@ -1,40 +1,46 @@
 <html>
-    <head>
-    
+
+<head>
+
     <link rel="stylesheet" type="text/css" href="../css/bootstrap4/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="../css/main_styles.css">
-    
+    <link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
+   
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.structure.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.theme.css">
+
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+        // Load google charts
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.setOnLoadCallback(drawChart);
 
-// Draw the chart and set the chart values
- function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['Task', 'Hours per Day'],
-  ['ข้าวเหนียว', 10],
-  ['ข้าวหอมมะลิ', 2],
-  ['ข้าวกล้อง', 8]
-  
-]);
+        // Draw the chart and set the chart values
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Task', 'Hours per Day'],
+                ['ข้าวเหนียว', 10],
+                ['ข้าวหอมมะลิ', 2],
+                ['ข้าวกล้อง', 8]
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'ยอดขายต่อเดือน', 'width':500, 'height':400};
+            ]);
 
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
+            // Optional; add a title and set the width and height of the chart
+            var options = { 'title': 'ยอดขายต่อเดือน', 'width': 500, 'height': 400 };
 
-</script>
+            // Display the chart inside the <div> element with id="piechart"
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            chart.draw(data, options);
+        }
 
-    </head>
+    </script>
 
-    <body>
-   
+</head>
+
+<body>
+
     <div class="super_container">
         <header class="header">
             <div class="container">
@@ -46,7 +52,7 @@ google.charts.setOnLoadCallback(drawChart);
                             </div>
                             <div class="main_nav">
                                 <ul>
-                                    <li class="active"><a href="../../first.php">หน้าหลัก</a></li>
+                                    <li class="active"><a href="../../index.php">หน้าหลัก</a></li>
                                     <li><a href="#">เกี่ยวกับเรา</a></li>
                                     <li><a href="#">ติดต่อเรา</a></li>
                                 </ul>
@@ -57,133 +63,161 @@ google.charts.setOnLoadCallback(drawChart);
             </div>
         </header>
     </div>
-<!---- header---->
+    <!---- header---->
 
-<br>
-<br>
-<br>
-<br>
-<div style="padding-left :40px;">
-<a href="after_login_sell.php"><button type="button" class="btn btn-primary" style="width:120px;">< ย้อนกลับ</button></a>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div style="padding-left :40px;">
+        <a href="after_login_sell.php"><button type="button" class="btn btn-primary" style="width:120px;">
+                ย้อนกลับ</button> </a> <br>
         <br>
-        <br>
 
-</div>
-<div class="row">
+    </div>
+    <div class="row">
 
-    <div class="col-8"  style="padding-left:120px;">
-        <b>ลงขายสินค้า</b>
+        <div class="col-8" style="padding-left:120px;">
+            <b>ลงขายสินค้า</b>
+        </div>
+        <div class="col-4">
+            ยอดขายรวม &nbsp;&nbsp;&nbsp;
+            <button type="button" id="cal_price" class="btn btn-primary" style="width:250px;">ระบบคำนวณรายได้ที่ต้องการ</button>
+        </div>
     </div>
-    <div class="col-4" >
-       ยอดขายรวม &nbsp;&nbsp;&nbsp;
-       <button type="button" class="btn btn-primary" style="width:250px;">ระบบคำนวณรายได้ที่ต้องการ</button>
-    </div>
-</div>
-<hr>
- <div  class="row">
+    <hr>
+    <div class="row">
         <div class="col-3">
             <div id="piechart"></div>
         </div>
         <div class="col-1">
-            
+
         </div>
         <div class="col-8">
             <br>
             <br>
-                    <table class="table">
+            <table class="table">
                 <thead>
-                <tr align="center">
-                    <th>วันที่</th>
-                    <th>รายการ</th>
-                    <th>จำนวน</th>
-                    <th>ราคาต่อหน่วย</th>
-                </tr>
+                    <tr align="center">
+                        <th>วันที่</th>
+                        <th>รายการ</th>
+                        <th>จำนวน</th>
+                        <th>ราคาต่อหน่วย</th>
+                    </tr>
                 </thead>
                 <tbody align="center">
-                <tr>
-                    <td>15/10/2018</td>
-                    <td>ข้าวเหนียว</td>
-                    <td>5</td>
-                    <td>150.00</td>
-                  
-                </tr>
-                <tr>
-                    <td>15/10/2018</td>
-                    <td>ข้าวกล้อง</td>
-                    <td>3</td>
-                    <td>220.00</td>
-                </tr>
-                <tr>
-                    <td>15/10/2018</td>
-                    <td>ข้าวหอมมะลิ</td>
-                    <td>10</td>
-                    <td>170.00</td>
-                </tr>
+                    <tr>
+                        <td>15/10/2018</td>
+                        <td>ข้าวเหนียว</td>
+                        <td>5</td>
+                        <td>150.00</td>
+
+                    </tr>
+                    <tr>
+                        <td>15/10/2018</td>
+                        <td>ข้าวกล้อง</td>
+                        <td>3</td>
+                        <td>220.00</td>
+                    </tr>
+                    <tr>
+                        <td>15/10/2018</td>
+                        <td>ข้าวหอมมะลิ</td>
+                        <td>10</td>
+                        <td>170.00</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
-</div>
-<br>
-<br>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
 
-                       
-        <footer class="container-fluid text-center foot" style="width:100%;padding-right: 0px;">
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
-                        <div class="copyrights">
-                                <img class="footimg"  src="../images/logo.png" alt="Logo">
-                            <hr class="dark-line">
-                                                <h5 class="active">อีเมล : ricehere.buu@gmail.com </h5>
-                                                <h5 class="active">โทร : 0853939908</h5>
-                            <p class="white-txt small">
-                                ซื้อง่าย ขายข้าว จากชาวนาคุณภาพ นึกถึง Rice Here 
-                            </p>
-                        </div>
-                </div>
-            <div class="col-4"></div>
-             </div>
-	    </footer>                    
-    
-  
+    <div id="login_div" class="main-div">
+        <div id="box" title="Basic dialog">
+            <div class="container" align="center">
+                <div class="email"><b>รายได้ที่ต้องการ</b></div>
+                <input class="email_user" id="profit" type="text" name="uname">
+                <div>ชนิดสินค้า</div>
+                <select class="select_buy" style=" width:70%">
+                 <option value="คิวบ์"></option>
+                    <option value="คิวบ์">ข้าวหอมมะลิ</option>
+                    <option value="กระสอบ">ข้าวเหนียว</option>
+                    <option value="ตัน">ข้าวเพื่อสุขภาพ</option>
+                </select>
+                
+                <div>ขนาดสินค้า</div>
+                <select class="select_buy" style=" width:70%">
+                 <option value="คิวบ์"></option>
+                    <option value="คิวบ์">คิวบ์</option>
+                    <option value="กระสอบ">กระสอบ</option>
+                    <option value="ตัน">ตัน</option>
+                </select>
+                
+                <div class="row">
+                            
+                            <div class="col-6" align="center" >
+                            <div>จำนวนสินค้า</div>
+                            <div class="div1"></div></div>
+                            
+                            <div class="col-6">
+                            <div>จะขายได้ถายใน</div>
+                            <div class="div2"></div>
+                         </div>
+                  </div>
+                <br>
+                <button  class="btn btn-warning" align="center" id="Sign_up1">คำนวน</button>
 
-        
-    </body>
-    
-    </html>
-
-
-    <!---
-
-    <div>
-        <form action="">
-            <input type="text" name="FirstName" value=""> บาท <br>
-            <select>
-                <option value="">ข้าวหอมมะลิ</option>
-                <option value="">ข้าวเหนียว</option>
-                <option value="">ข้าวเพื่อสุขภาพ</option>
-                <option value="">ข้าวขาว</option>
-            </select>
-            <br>
-        </form>
-        <div class="row">
-            <div class="col-6">
-                <fieldset>
-                    จำนวนสินค้าที่ต้องขาย
-                    <br>
-                    <br>
-                </fieldset>
-            </div>
-            <div class="col-6">
-                <fieldset>
-                    จะขายได้ภายใน
-                    <br>
-                    <br>
-                </fieldset>
             </div>
         </div>
-
     </div>
 
 
-    --->
+    <footer class="container-fluid text-center foot" style="width:100%;padding-right: 0px;">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="copyrights">
+                    <img class="footimg" src="../images/logo.png" alt="Logo">
+                    <hr class="dark-line">
+                    <h5 class="active">อีเมล : ricehere.buu@gmail.com </h5>
+                    <h5 class="active">โทร : 0853939908</h5>
+                    <p class="white-txt small">
+                        ซื้อง่าย ขายข้าว จากชาวนาคุณภาพ นึกถึง Rice Here
+                    </p>
+                </div>
+            </div>
+            <div class="col-4"></div>
+        </div>
+    </footer>
+
+
+
+
+</body>
+<script type="text/javascript" src="../../js/jquery.js"></script>
+<script type="text/javascript" src="../../js/jquery-ui.js"></script>
+<script type="text/javascript" src="../../js/jquery-ui.min.js"></script>
+<script>
+    $(function () {
+        $("#box").dialog({
+            autoOpen: false,
+            title: 'กรุณากรอกข้อมูล',
+            width: 400,
+            height: 550,
+            modal: true,
+            resizable: false,
+            draggable: false,
+        });
+        $("#cal_price").on("click", function () {
+            $("#box").dialog("open");
+        });
+        $("#Sign_up1").on("click", function () {
+            $("#box").dialog("close");
+        });
+    });
+
+</script>
+
+</html>
